e0e2c33f82a91ede6ef5a58497eb31d4dc0338b1
SeBin Nam <nsb2519@e-mirim.hs.kr>
2016-06-02 19:25:55 +0900

    #1 Edit UI Design

--- app/src/main/java/kr/hs/emirim/sebin2519/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sebin2519/simplewebbrowser/MainActivity.java
@@ -2,12 +2,42 @@ package kr.hs.emirim.sebin2519.simplewebbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    //위젯을 멤버필드에 선언
+    EditText editUrl;
+    Button butMove, butPrev;
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //참조변수 , 캐스팅: 형변환
+        editUrl=(EditText) findViewById(R.id.edit_url);
+        butMove=(Button) findViewById(R.id.but_move);
+        butPrev=(Button) findViewById(R.id.but_prev);
+        web=(WebView)findViewById(R.id.web);
+
+        //이벤트 핸들러(익명클래스, 형제클래스) => 형제 클래스로!
+        butMove.setOnClickListener(this);
+
+        //이벤트 소스 => butMove(이벤트: OnClick)
+
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
     }
 }
